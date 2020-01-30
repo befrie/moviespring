0352f0de2bc78ed44e2cc3f514969445f7be0da2
befrie <befrie@arcor.de>
2020-01-30 15:34:11 +0100

    Initial commit

---
+++ src/main/java/de/befrie/dvega/DvegaApplication.java
@@ -0,0 +1,13 @@
+package de.befrie.dvega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvegaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/befrie/dvega/DvegaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.befrie.dvega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
